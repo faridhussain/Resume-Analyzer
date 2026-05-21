18f9def94a8b890ae9ccb7351c8c491e4fde8aa6
farhandotme <princeofficial355@gmail.com>
2026-05-21 07:59:33 +0530

    created the models in schema.prisma

--- app_backend/index.ts
+++ app_backend/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use();
+app.get("/", (req, res) => {
+  res.json({ message: "hello there! This is the node js backend" });
+});
+app.listen(3000, () => {
+  console.log("running on port 3000");
+});
