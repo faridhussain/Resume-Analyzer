64ec697e244c88ca342c50b55d984d5740522466
Farid Hussain <faridhussain0011@gmail.com>
2026-06-11 20:21:38 +0530

    add login page

--- app_frontend/src/App.jsx
+++ app_frontend/src/App.jsx
@@ -1,9 +1,12 @@
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
+
+import Login from './pages/Login';
 
 export default function App() {
   return (
     <div className="min-h-screen w-full font-geometria selection:bg-yellow-400/20 selection:text-white">
-      <Home />
+      {/* <Home /> */}
+      <Login />
     </div>
   );
 }
